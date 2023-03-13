f8cb4ecf5a2325e920a1679088efed8ef7aed161
Noble Mungu <mungunoble@gmail.com>
2023-03-13 06:15:27 -0700

    Add required form validators

--- register.aspx
+++ register.aspx
@@ -4,11 +4,19 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
     <asp:Label ID="Label1" class="label" runat="server" Text="Full name"></asp:Label><br />
-    <asp:TextBox ID="TextBox1" class="input" runat ="server"></asp:TextBox><br />
+    <asp:TextBox ID="TxtFullname" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TxtFullname" ErrorMessage="Full name required!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+<br />
     <asp:Label ID="Label2" runat="server" class="label" Text="User name"></asp:Label><br />
-   <asp:TextBox ID="TextBox2" class="input" runat ="server"></asp:TextBox><br />
+   <asp:TextBox ID="TxtUsername" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TxtUsername" ErrorMessage="User name required!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+<br />
     <asp:Label ID="Label3" runat="server" class="label" Text="Email Address"></asp:Label><br />
-    <asp:TextBox ID="TextBox3" class="input" runat ="server"></asp:TextBox><br />
+    <asp:TextBox ID="TxtEmail" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="TxtEmail" ErrorMessage="Email required!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+    <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="TxtEmail" ErrorMessage="Valid Email must have @ symbol" ForeColor="Red" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" ValidationGroup="Vg">*</asp:RegularExpressionValidator>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator8" runat="server" ControlToValidate="TxtEmail" ErrorMessage="RequiredFieldValidator" ValidationGroup="Vg"></asp:RequiredFieldValidator>
+<br />
     <p>Select your gender:</p>
     <asp:RadioButtonList ID="RadioButtonList1" runat="server">
         <asp:ListItem Value="Male"></asp:ListItem>
@@ -22,13 +30,24 @@
         <asp:ListItem Value="Tanzania"></asp:ListItem>
     </asp:DropDownList><br />
     <asp:Label ID="Label5" runat="server" class="label" Text="Mobile Number"></asp:Label><br />
-    <asp:TextBox ID="TextBox4" class="input" runat ="server"></asp:TextBox><br />
+    <asp:TextBox ID="TxtMobile" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ControlToValidate="TxtMobile" ErrorMessage="Mobile number is required!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+<br />
     <asp:Label ID="Label6" runat="server" class="label" Text="Password"></asp:Label><br />
-  <asp:TextBox ID="TextBox5" class="input" runat ="server"></asp:TextBox><br />
+  <asp:TextBox ID="Txtpass1" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator6" runat="server" ControlToValidate="Txtpass1" ErrorMessage="Password is required!!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+<br />
     <asp:Label ID="Label8" runat="server" class="label" Text="Confirm Password"></asp:Label><br />
-   <asp:TextBox ID="TextBox6" class="input" runat ="server"></asp:TextBox><br />
-    <asp:Button ID="Button1" runat="server" class="btn" Text="Create account" />
+   <asp:TextBox ID="TxtPass2" class="input" runat ="server" ValidationGroup="Vg"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator7" runat="server" ControlToValidate="TxtPass2" ErrorMessage="Password is required!!" ForeColor="Red" ValidationGroup="Vg">*</asp:RequiredFieldValidator>
+<br />
+    <asp:Button ID="Button1" runat="server" class="btn" Text="Create account" ValidationGroup="Vg" />
     <asp:Button ID="Button2" runat="server" class="btn" Text="Cancel" />
+
+
+
+    <br />
+    <asp:ValidationSummary ID="ValidationSummary1" runat="server" ForeColor="Red" ValidationGroup="Vg" />
 
 
 
--- register.aspx.designer.cs
+++ register.aspx.designer.cs
@@ -24,13 +24,22 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// TextBox1 control.
+        /// TxtFullname control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.TextBox TxtFullname;
+
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
 
         /// <summary>
         /// Label2 control.
@@ -42,13 +51,22 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// TextBox2 control.
+        /// TxtUsername control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        protected global::System.Web.UI.WebControls.TextBox TxtUsername;
+
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
 
         /// <summary>
         /// Label3 control.
@@ -60,13 +78,40 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label3;
 
         /// <summary>
-        /// TextBox3 control.
+        /// TxtEmail control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        protected global::System.Web.UI.WebControls.TextBox TxtEmail;
+
+        /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
+        /// RegularExpressionValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+
+        /// <summary>
+        /// RequiredFieldValidator8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator8;
 
         /// <summary>
         /// RadioButtonList1 control.
@@ -96,13 +141,22 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label5;
 
         /// <summary>
-        /// TextBox4 control.
+        /// TxtMobile control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        protected global::System.Web.UI.WebControls.TextBox TxtMobile;
+
+        /// <summary>
+        /// RequiredFieldValidator5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
 
         /// <summary>
         /// Label6 control.
@@ -114,13 +168,22 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label6;
 
         /// <summary>
-        /// TextBox5 control.
+        /// Txtpass1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+        protected global::System.Web.UI.WebControls.TextBox Txtpass1;
+
+        /// <summary>
+        /// RequiredFieldValidator6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator6;
 
         /// <summary>
         /// Label8 control.
@@ -132,13 +195,22 @@ namespace WebApplication8
         protected global::System.Web.UI.WebControls.Label Label8;
 
         /// <summary>
-        /// TextBox6 control.
+        /// TxtPass2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+        protected global::System.Web.UI.WebControls.TextBox TxtPass2;
+
+        /// <summary>
+        /// RequiredFieldValidator7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator7;
 
         /// <summary>
         /// Button1 control.
@@ -157,5 +229,14 @@ namespace WebApplication8
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// ValidationSummary1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
     }
 }
